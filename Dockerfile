e2b70bcfe697b511a4597abc70ecc6a582383f0f
alanouda2 <alanoud2@andrew.cmu.edu>
2023-10-19 17:39:26 +0300

    Hopefully it works

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,18 @@ FROM python:3.9
 WORKDIR /code
 
 # TODO
+
+# Copy the requirements file into the container at /code
+COPY requirements.txt /code/
+
+# Install any needed packages specified in requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the rest of your application code into the container
+COPY . /code/
+
+# Expose the port on which your FastAPI application will run. By default, FastAPI runs on port 8000.
+EXPOSE 8000
+
+# Specify the command to run your FastAPI application. You can use uvicorn to run your FastAPI app.
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/main.py
+++ app/main.py
@@ -3,43 +3,36 @@ import requests
 
 app = FastAPI()
 
+RECITATION_HOURS = {"a": "09:00~09:50", "b": "10:00~10:50", "c": "11:00~11:50", "d": "12:00~12:50"}
+MICROSERVICE_LINK = "https://whos-my-ta.fly.dev/section_id/"
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
-RECITATION_HOURS = {"a": "09:00~09:50", "b": "10:00~10:50",
-                    "c": "11:00~11:50", "d": "12:00~12:50"}
-MICROSERVICE_LINK = "https://whos-my-ta.fly.dev/section_id/"
-
-
 @app.get("/section_info/{section_id}")
 def get_section_info(section_id: str):
-
     if section_id is None:
         raise HTTPException(status_code=404, detail="Missing section id")
 
     section_id = section_id.lower()
-
-    response = requests.get("https://whos-my-ta.fly.dev/section_id/" + section_id)
-
-    # You can check out what the response body looks like in terminal using the print statement
+    response = requests.get(MICROSERVICE_LINK + section_id)
     data = response.json()
-    print(data)
+
+    if "ta_names" not in data:
+        raise HTTPException(status_code=404, detail="TA names not found for the section")
+
     ta_name_list = data["ta_names"]
     ta1_name = ta_name_list[0]["fname"] + " " + ta_name_list[0]["lname"]
     ta2_name = ta_name_list[1]["fname"] + " " + ta_name_list[1]["lname"]
 
-    print(ta1_name)
-
-    # TODO
-    if section_id == "a":
+    if section_id in RECITATION_HOURS:
+        start_time, end_time = RECITATION_HOURS[section_id].split("~")
         return {
-            "section": "section_name",
-            "start_time": "HH:MM",
-            "end_time": "HH:MM",
-            "ta": ["taName1", "taName2"]
+            "section": section_id,
+            "start_time": start_time,
+            "end_time": end_time,
+            "ta": [ta1_name, ta2_name]
         }
     else:
         raise HTTPException(status_code=404, detail="Invalid section id")
